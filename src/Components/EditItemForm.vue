8a41b6a6d4fab6ec4c8aa9e35e9741a3c76ad04d
Henry Nguyen <knguyen@icstars.org>
2024-10-10 01:08:54 -0500

    pull item form out into its own componenent

--- src/Components/EditItemForm.vue
+++ src/Components/EditItemForm.vue
@@ -1,9 +1,161 @@
-<script>
-// stuff
+<script setup>
+import { ref, watch } from "vue";
+import CurrencyField from "./CurrencyField.vue";
+
+const generateNewId = () => Math.floor(Math.random() * 90000) + 10000;
+
+// Define props
+const props = defineProps({
+  categoryList: {
+    type: Array,
+    required: true,
+  },
+  selectedItem: {
+    type: Object,
+    required: true,
+  },
+  displayForm: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+// Define emits
+const emit = defineEmits([
+  "handleSubmit",
+  "handleDelete",
+  "update:displayForm",
+]);
+
+const displayDelete = ref(false);
+const formFields = ref({
+  name: "",
+  qty: "",
+  id: generateNewId(),
+  price: "",
+  category: "",
+});
+
+watch(() => {
+  displayDelete.value = !!props.selectedItem?.name;
+
+  // keep the ref up to date with what the props is
+  formFields.value = {
+    name: props.selectedItem?.name,
+    qty: props.selectedItem?.qty,
+    id: props.selectedItem?.id,
+    price: props.selectedItem?.price,
+    category: props.selectedItem?.category,
+  };
+});
+
+const addOrDeleteItem = (isAdding) => {
+  const itemObj = {
+    itemId: formFields.value.id,
+    itemName: formFields.value.name,
+    quantity: formFields.value.qty,
+    pricePerUnit: formFields.value.price,
+  };
+
+  if (isAdding) {
+    emit("handleSubmit", itemObj, formFields.value.category);
+  } else {
+    emit("handleDelete", itemObj, formFields.value.category);
+  }
+
+  // close form
+  emit("update:displayForm", false);
+};
+
+const onSubmit = () => {
+  addOrDeleteItem(true);
+};
+
+const onDelete = () => {
+  addOrDeleteItem(false);
+};
 </script>
 
-<template>stuff</template>
+<template>
+  <v-form @submit.prevent="onSubmit" v-if="displayForm" class="yHeight">
+    <div class="form-content">
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-btn
+            @click="emit('update:displayForm', false)"
+            color="primary"
+            variant="text"
+            icon
+          >
+            <v-icon icon="mdi-close"></v-icon>
+          </v-btn>
+        </v-col>
 
-<style>
-/* stuff */
+        <v-col cols="12" md="6" class="pa-2">
+          <v-text-field
+            color="primary"
+            label="Item name"
+            v-model="formFields.name"
+          />
+        </v-col>
+        <v-col cols="12" md="6" class="pa-2">
+          <v-text-field
+            v-model="formFields.id"
+            readonly
+            color="primary"
+            label="Item ID"
+          />
+        </v-col>
+        <v-col cols="12" md="6" class="pa-2">
+          <v-select
+            v-model="formFields.category"
+            label="Category"
+            :items="categoryList"
+          />
+        </v-col>
+        <v-col cols="12" md="6" class="pa-2">
+          <CurrencyField
+            v-model:modelValue="formFields.price"
+            label="Price per unit"
+          />
+        </v-col>
+        <v-col cols="12" md="6" class="pa-2">
+          <v-text-field
+            v-model="formFields.qty"
+            color="primary"
+            label="Quantity"
+            type="number"
+          />
+        </v-col>
+      </v-row>
+    </div>
+
+    <v-row no-gutters justify="center" class="action-buttons">
+      <v-col cols="6" class="pa-1">
+        <v-btn color="error" v-if="displayDelete" @click="onDelete" block>
+          Delete
+        </v-btn>
+      </v-col>
+      <v-col cols="6" class="pa-1">
+        <v-btn color="primary" block type="submit">Save</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<style scoped>
+.yHeight {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.form-content {
+  flex: 1;
+  overflow: auto;
+  padding: 4px 8px 8px 8px;
+}
+.action-buttons {
+  flex: 0 0 auto;
+  padding: 0px 4px 16px 4px;
+}
 </style>
--- src/views/Inventory/Inventory.vue
+++ src/views/Inventory/Inventory.vue
@@ -1,5 +1,5 @@
 <script setup>
-import CurrencyField from "@/Components/CurrencyField.vue";
+import EditItemForm from "@/Components/EditItemForm.vue";
 import PageLayout from "@/Components/PageLayout.vue";
 import { computed, ref } from "vue";
 import { useDisplay } from "vuetify";
@@ -7,71 +7,46 @@ import CategorySelection from "../../Components/CategorySelection.vue";
 import ItemSelection from "../../Components/ItemSelection.vue";
 import { useInventoryListStore } from "../../store/inventoryList";
 
-const generateId = Math.floor(Math.random() * 90000) + 10000;
+const generateNewId = () => Math.floor(Math.random() * 90000) + 10000;
 
 const { width } = useDisplay();
 const inventoryStore = useInventoryListStore();
 
 const currentCategory = ref("All");
-const displayForm = ref(false);
-const displayDelete = ref(false);
-const name = ref("");
-const qty = ref();
-const id = ref("");
-const price = ref();
-const category = ref("");
+const selectedItem = ref({});
+const displayItemForm = ref(false);
+// const displayCategoryForm
 
-const changePageLayout = computed(() => {
-  return width.value < 960;
-});
+const changePageLayout = computed(() => width.value < 960);
+const editLabel = computed(() => selectedItem.value?.name);
 
-const onItemSelection = (item) => {
-  displayForm.value = true;
-  displayDelete.value = item ? true : false;
-  // set to ref for adding or editing
-  name.value = item?.itemName || "";
-  qty.value = item?.quantity || 0;
-  price.value = item?.pricePerUnit.toString() || "";
-  id.value = item?.itemId || generateId.toString();
+const categoryList = inventoryStore.value.map((cat) => cat.categoryName);
 
-  const findCategory = inventoryStore.value.find((category) =>
-    category.items.some(
+const handleItemSelection = (item) => {
+  const foundCategory = inventoryStore.value.find((cat) =>
+    cat.items.some(
       (i) => i.itemName === item?.itemName && i.itemId === item?.itemId
     )
   );
-  category.value = findCategory?.categoryName || "";
-};
 
-const addOrDeleteItem = (isAdd) => {
-  const itemObj = {
-    itemId: id.value,
-    itemName: name.value,
-    quantity: qty.value,
-    pricePerUnit: price.value,
+  displayItemForm.value = true;
+
+  selectedItem.value = {
+    name: item?.itemName || "",
+    qty: (item?.quantity === 0 ? "0" : item?.quantity) || "",
+    id: item?.itemId || generateNewId(),
+    price: item?.pricePerUnit?.toString() || "",
+    category: foundCategory?.categoryName || "",
   };
-  if (isAdd) {
-    inventoryStore.addItem(itemObj, category.value);
-  } else {
-    inventoryStore.deleteItem(itemObj, category.value);
-  }
-
-  displayForm.value = false;
-};
-
-const onSubmit = () => {
-  addOrDeleteItem(true);
-};
-
-const onDeleteItem = () => {
-  // TODO: new a confirm modal or something here
-  addOrDeleteItem(false);
 };
 </script>
 
 <template>
   <PageLayout :displayIcon="changePageLayout">
     <template #iconHeader>
-      <v-btn height="48" v-if="changePageLayout">Edit: {{ name }}</v-btn>
+      <v-btn height="48" v-if="changePageLayout" :disabled="!editLabel"
+        >Edit: {{ editLabel }}</v-btn
+      >
     </template>
 
     <CategorySelection
@@ -83,93 +58,19 @@ const onDeleteItem = () => {
       v-model:currentCategory="currentCategory"
       :inventoryStore="inventoryStore.value"
       :isEditing="true"
-      @onItemSelection="onItemSelection"
+      @onItemSelection="handleItemSelection"
     />
+
     <v-col md="4" cols="12" v-if="!changePageLayout" class="fullHeight">
       <v-container fluid class="pb-1 px-2 fullHeight">
         <v-sheet height="100%" elevation="4" rounded class="yHeight">
-          <!-- hide when item is not selected -->
-          <!-- TODO: this is gonna be its own component, the form -->
-          <v-form
-            @submit.prevent="onSubmit()"
-            class="yHeight"
-            v-if="displayForm"
-          >
-            <div style="flex: 1; overflow: auto; padding: 4px 8px 8px 8px">
-              <v-row no-gutters>
-                <v-col cols="12">
-                  <v-btn
-                    @click="displayForm = false"
-                    color="primary"
-                    variant="text"
-                    icon
-                  >
-                    <v-icon icon="mdi-close"></v-icon>
-                  </v-btn>
-                </v-col>
-
-                <v-col cols="12" md="6" class="pa-2">
-                  <v-text-field
-                    color="primary"
-                    label="Item name"
-                    v-model="name"
-                  />
-                </v-col>
-                <v-col cols="12" md="6" class="pa-2">
-                  <v-text-field
-                    v-model="id"
-                    readonly
-                    color="primary"
-                    label="Item ID"
-                  />
-                </v-col>
-                <v-col cols="12" md="6" class="pa-2">
-                  <v-select
-                    v-model="category"
-                    label="Category"
-                    :items="
-                      inventoryStore.value.map(
-                        (category) => category.categoryName
-                      )
-                    "
-                  />
-                </v-col>
-                <v-col cols="12" md="6" class="pa-2">
-                  <CurrencyField
-                    v-model:modelValue="price"
-                    label="Price per unit"
-                  />
-                </v-col>
-                <v-col cols="12" md="6" class="pa-2">
-                  <v-text-field
-                    v-model="qty"
-                    color="primary"
-                    label="Quantity"
-                    type="number"
-                  />
-                </v-col>
-              </v-row>
-            </div>
-
-            <v-row
-              no-gutters
-              justify="center"
-              style="flex: 0 0 auto; padding: 0px 4px 16px 4px"
-            >
-              <v-col cols="6" class="pa-1">
-                <v-btn
-                  color="error"
-                  block
-                  @click="onDeleteItem"
-                  v-if="displayDelete"
-                  >Delete</v-btn
-                >
-              </v-col>
-              <v-col cols="6" class="pa-1">
-                <v-btn color="primary" block type="submit">Save</v-btn>
-              </v-col>
-            </v-row>
-          </v-form>
+          <EditItemForm
+            :categoryList="categoryList"
+            :selectedItem="selectedItem"
+            v-model:displayForm="displayItemForm"
+            @handleSubmit="inventoryStore.addItem"
+            @handleDelete="inventoryStore.deleteItem"
+          />
         </v-sheet>
       </v-container>
     </v-col>
@@ -182,7 +83,7 @@ const onDeleteItem = () => {
   margin: 0;
 }
 .yHeight {
-  display: flex; /* Use flexbox */
+  display: flex;
   flex-direction: column;
   height: 100%;
 }
